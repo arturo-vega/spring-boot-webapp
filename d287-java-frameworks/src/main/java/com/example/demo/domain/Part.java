57f7cb17eca9e6bf62a1dadc65c59d79402b67fb
arturo-vega <166672631+arturo-vega@users.noreply.github.com>
2024-04-15 19:46:12 -0500

    Add files via upload

---
+++ d287-java-frameworks/src/main/java/com/example/demo/domain/Part.java
@@ -0,0 +1,117 @@
+package com.example.demo.domain;
+
+import com.example.demo.validators.ValidDeletePart;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ *
+ *
+ *
+ */
+@Entity
+@ValidDeletePart
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="part_type",discriminatorType = DiscriminatorType.INTEGER)
+@Table(name="Parts")
+public abstract class Part implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+    String name;
+    @Min(value = 0, message = "Price value must be positive")
+    double price;
+    @Min(value = 0, message = "Inventory value must be positive")
+    int inv;
+
+    int invMax;
+    int invMin;
+
+    @ManyToMany
+    @JoinTable(name="product_part", joinColumns = @JoinColumn(name="part_id"),
+            inverseJoinColumns=@JoinColumn(name="product_id"))
+    Set<Product> products= new HashSet<>();
+
+    public Part() {
+    }
+
+
+    public boolean isInvValid (int inv, int invMax, int invMin) {
+        return inv >= invMin && inv <= invMax;
+    }
+
+    public int getInvMax() {return invMax;}
+    public int getInvMin() {return invMin;}
+    public void setInvMax(int invMax) {this.invMax = invMax;}
+    public void setInvMin(int invMin) {this.invMin = invMin;}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getInv() {
+        return inv;
+    }
+
+    public void setInv(int inv) {this.inv = inv;}
+
+    public void checkInv() {
+        if (inv < invMin) {
+            inv = invMin;
+        }
+        else if (inv > invMax) {
+            inv = invMax;
+        }
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Part part = (Part) o;
+
+        return id == part.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
